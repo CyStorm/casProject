b9781dd4f4a95e17584cf024d5a4eb6781539990
fumblevore <36111845+fumblevore@users.noreply.github.com>
2018-08-13 15:16:27 -0400

    please title and document changes
    added a button that prints to log lol.

--- masterCasProject/app/src/main/java/com/example/fumblevore_gaming/mastercasproject/MainActivity.java
+++ masterCasProject/app/src/main/java/com/example/fumblevore_gaming/mastercasproject/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button chadTestButton = findViewById(R.id.chadTestButton);
+
+        chadTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("chadTest", "button is work");
+            }
+        });
     }
 //yes
     @Override
@@ -34,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
