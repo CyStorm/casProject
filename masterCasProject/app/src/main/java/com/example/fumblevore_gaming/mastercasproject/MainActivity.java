1d36e9b220e76481fded4b84c688316c38c2b955
idonlakyo <daniel.lisq12@gmail.com>
2019-03-14 18:19:52 -0400

    fragments
    moved calendar into fragment
    made space for main screen
    added blank todolist fragment

--- masterCasProject/app/src/main/java/com/example/fumblevore_gaming/mastercasproject/MainActivity.java
+++ masterCasProject/app/src/main/java/com/example/fumblevore_gaming/mastercasproject/MainActivity.java
@@ -1,28 +1,30 @@
 package com.example.fumblevore_gaming.mastercasproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+<<<<<<< HEAD
+import android.view.Menu;
+=======
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 import java.util.Calendar;
 import java.util.Date;
 import com.squareup.timessquare.CalendarPickerView;
+>>>>>>> 32b10c8b433ce8eaf70fa57a32cf412b6c9ca13b
 
-// daniel
     public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
+<<<<<<< HEAD
+=======
         private Button chatTestbutton;
         private DrawerLayout drawer;
 
@@ -48,6 +50,7 @@ import com.squareup.timessquare.CalendarPickerView;
                         new CalendarFragment()).commit();
                 navigationView.setCheckedItem(R.id.nav_calendar);
             }
+            
             Date today = new Date();
             Calendar nextYear = Calendar.getInstance();
             nextYear.add(Calendar.YEAR, 1);
@@ -55,42 +58,66 @@ import com.squareup.timessquare.CalendarPickerView;
             CalendarPickerView datePicker = findViewById(R.id.calendar_view);
             datePicker.init(today, nextYear.getTime())
                     .withSelectedDate(today);
+>>>>>>> 32b10c8b433ce8eaf70fa57a32cf412b6c9ca13b
 
-            datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
-                @Override
-                public void onDateSelected(Date date) {
-                    //String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-                    Calendar calSelected = Calendar.getInstance();
-                    calSelected.setTime(date);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-                    String selectedDate = "" + calSelected.get(Calendar.DAY_OF_MONTH)
-                            + " " + (calSelected.get(Calendar.MONTH) + 1)
-                            + " " + calSelected.get(Calendar.YEAR);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+<<<<<<< HEAD
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close); 
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected( @NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_calendar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CalendarFragment()).commit();
+                break;
+            case R.id.nav_todolist:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ToDoListFragment()).commit();
+                break;
+=======
                     Toast.makeText(MainActivity.this, selectedDate, Toast.LENGTH_SHORT).show();
                 }
+
                 @Override
                 public void onDateUnselected(Date date) {
 
                 }
         });
+       
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 32b10c8b433ce8eaf70fa57a32cf412b6c9ca13b
+=======
+>>>>>>> 32b10c8b433ce8eaf70fa57a32cf412b6c9ca13b
+=======
+>>>>>>> 32b10c8b433ce8eaf70fa57a32cf412b6c9ca13b
         }
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.nav_calendar:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            new CalendarFragment()).commit();
-                    break;
-                case R.id.nav_message:
-                    Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
-                    break;
-            }
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-            return true;
+        } else {
+            super.onBackPressed();
         }
 
+<<<<<<< HEAD
+=======
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             // Inflate the menu; this adds items to the action bar if it is present.
@@ -121,5 +148,12 @@ import com.squareup.timessquare.CalendarPickerView;
 
             return super.onOptionsItemSelected(item);
         }
+
+<<<<<<< HEAD
+>>>>>>> 32b10c8b433ce8eaf70fa57a32cf412b6c9ca13b
+=======
+>>>>>>> 32b10c8b433ce8eaf70fa57a32cf412b6c9ca13b
+        
     }
+}
 
