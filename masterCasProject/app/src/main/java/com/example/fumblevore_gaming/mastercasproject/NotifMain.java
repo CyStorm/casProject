2b6f8ca0e91c4e134733e73a282aad3df83b7e19
nadnerbb <brendan_hu@live.com>
2019-04-16 20:51:29 -0400

    Added NotifMain

---
+++ masterCasProject/app/src/main/java/com/example/fumblevore_gaming/mastercasproject/NotifMain.java
@@ -0,0 +1,8 @@
+package com.example.fumblevore_gaming.mastercasproject;
+
+import android.app.AlarmManager;
+import android.os.Bundle;
+
+import com.example.fumblevore_gaming.mastercasproject.R;
+
+
